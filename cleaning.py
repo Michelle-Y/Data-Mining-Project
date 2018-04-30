5b9d9ba9eda0e696fd04cd27c3289e95ff112777
Nick Montoya <nimo0295@colorado.edu>
2018-04-29 21:19:04 -0600

    implemented vader scoring, working on clustering now

--- cleaning.py
+++ cleaning.py
@@ -6,6 +6,9 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import matplotlib.pylab as plt
 import re
 
+NEGATIVE = 0
+POSITIVE = 1
+NEUTRAL = 2
 
 def getFirstFile(loc):
     title = ('EmotionLevel', 'TwitterID', 'PostTime', 'IsQuery', 'Username', 'content')
@@ -26,10 +29,13 @@ def tokenizer(tweet_set):
     for sent in tweet_set:
         tmp_sent = tknzr.tokenize(sent)
         tknzd_tweets.append(tmp_sent)
+    return tknzd_tweets
+
+def VaderFormat(tweet_set):
     # Replace real URLs with 'URL'
     print ("Tweets tokenized, now removing URLs")
     compl_tweets = []
-    for sent in tknzd_tweets:
+    for sent in tweet_set:
         tmp_sent = []
         for word in sent:
             word = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', word)
@@ -40,13 +46,17 @@ def tokenizer(tweet_set):
 
 def VaderScore(tweet_set):
     sid = SentimentIntensityAnalyzer()
-    for sentence in tweet_set[:2]:
+    updated_list = []
+    for sentence in tweet_set:
         print (sentence)
         ss = sid.polarity_scores(sentence)
-        for k in sorted(ss):
-            print ('{0}: {1}, '.format(k, ss[k]), end='')
-        print()
-
+        if ss['compound'] >= 0.5:
+            updated_list.append((sentence, POSITIVE))
+        elif ss['compound'] <= -0.5:
+            updated_list.append((sentence, NEGATIVE))
+        else:
+            updated_list.append((sentence, NEUTRAL))
+    return updated_list
 
 if __name__ == "__main__":
     print ("Starting cleaning process")
@@ -63,3 +73,7 @@ if __name__ == "__main__":
     all_tweets, all_dates = getDateAndTweetList(small_set)
     print ("Now tokenizing the tweets")
     tknzd_tweets = tokenizer(all_tweets)
+    vader_tweets = VaderFormat(tknzd_tweets)
+    print("Scoring each tweet")
+    scored_tweets = VaderScore(vader_tweets[:10])
+    print(scored_tweets)
